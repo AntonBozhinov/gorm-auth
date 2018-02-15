43a161f58c69a8daefaba78caf3c3ac0ff05e1d2
Anton Bozhinov <anton.bozhinov@softwaregroup.com>
2018-02-15 17:23:53 +0200

    add roles and permission types

--- permission.go
+++ permission.go
@@ -1,1 +1,23 @@
 package auth
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+// Permissions slice of all role permissions
+type Permissions []*Permission
+
+// Permission is individual permission a role can has
+type Permission struct {
+	gorm.Model
+	Name string
+	Description string
+	CategoryID uint
+	Category PermissionCategory
+}
+
+// PermissionCategory is used to categorize permissions
+type PermissionCategory struct {
+	gorm.Model
+	Name string
+}
--- role.go
+++ role.go
@@ -1,1 +1,22 @@
 package auth
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+// RoleCategory is used to categorize roles to given entity. For example users organizations groups and so on...
+type RoleCategory struct {
+	gorm.Model
+	Name string
+	Description string
+}
+
+// Role is the user role holdining a set of permissions used for user authorization
+type Role struct {
+	gorm.Model
+	UserID uint
+	Name string
+	Description string
+	Category RoleCategory
+	CategoryID uint
+}
